e764bf6962c983e86f5871d550a02d3b59c0ff48
djavani <djavanirgomes@gmail.com>
2017-11-06 21:52:23 -0200

    Recuperando parâmetros da rota com Snapshot

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-oferta',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./oferta.component.css']
 })
 export class OfertaComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private route: ActivatedRoute) { 
+    
+  }
 
   ngOnInit() {
+    console.log('ID recuperado da rota:' + this.route.snapshot.params['id'])
   }
 
 }
