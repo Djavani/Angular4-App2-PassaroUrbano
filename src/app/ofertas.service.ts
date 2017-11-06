c279573a3cc8d65c8ff331affcc7e658d7031422
Djavani <djavanirgomes@gmail.com>
2017-11-06 11:50:44 -0200

    Recuperando ofertas do tipo Restaurantes (HTTP Resquest / API Rest / Promise)

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -20,4 +20,10 @@ export class OfertasService {
     
   }
 
+  public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]>{
+    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    .toPromise()
+    .then((resposta: any) => resposta.json())
+  }
+
 }
