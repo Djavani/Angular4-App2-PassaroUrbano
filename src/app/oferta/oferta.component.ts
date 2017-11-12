6baabb217c0b2862e8e2e8e24419229003077a41
djavani <djavanirgomes@gmail.com>
2017-11-12 15:11:20 -0200

    Incluindo o campo de pesquisa de ofertas

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,11 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { OfertasService } from './../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
-import { Observable } from 'rxjs/Observable';
-import { Observer } from 'rxjs/Observer';
-import { Subscription } from 'rxjs/Subscription';
-import 'rxjs/RX'
-
 
 @Component({
   selector: 'app-oferta',
@@ -16,9 +11,6 @@ import 'rxjs/RX'
 })
 export class OfertaComponent implements OnInit {
   
-  private tempoObservableSubscription : Subscription
-  private meuObservableTestSubscription : Subscription
-
   public oferta: Oferta
 
   constructor(
@@ -30,39 +22,11 @@ export class OfertaComponent implements OnInit {
     this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
     .then(( oferta: Oferta ) => {
       this.oferta = oferta
-    })/*
-    this.route.params.subscribe((parametro: any) => { console.log(parametro) },
-      (erro: any) => console.log(erro),
-      () => console.log('processamento foi classificado como concluido!')
-    )*/
-
-    
-    let tempo = Observable.interval(2000)
-
-    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
-      console.log(intervalo)
-    })
-  
-
-    //observable (observavel)
-    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
-      observer.next(1)
-      observer.next(5)
-      observer.complete()
-    })
-
-    //observable (observador)
-    this.meuObservableTestSubscription = meuObservableTeste.subscribe(
-      (resultado: number) => console.log(resultado + 10),
-      (erro: string) => console.log(erro),
-      () => console.log('Stream de eventos foi finalizada')
-    )
+    })    
     
   }
   
   ngOnDestroy(){
-    this.meuObservableTestSubscription.unsubscribe();
-    this.tempoObservableSubscription.unsubscribe();
   }
 
 }
