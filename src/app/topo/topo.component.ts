f675fd653bf337d2dfd81860d264adb554ef7722
djavani <djavanirgomes@gmail.com>
2017-11-12 23:05:58 -0200

    Capturando entrada do usuário - Approach: Variável de referência do template

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -13,7 +13,7 @@ export class TopoComponent implements OnInit {
   ngOnInit() {
   }
 
-  public pesquisa(event: Event): void{
-    console.log((<HTMLInputElement>event.target).value)
+  public pesquisa(termoDaBusca: string): void{
+    console.log(termoDaBusca)
   }
 }
