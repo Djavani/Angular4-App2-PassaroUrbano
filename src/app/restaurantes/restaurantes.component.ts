2b617b38e87a5c8c80bf9d6b07d3e1f046269d7f
Djavani <djavanirgomes@gmail.com>
2017-11-14 17:14:35 -0200

    Removendo pipe de data - efeito didatico apenas

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -11,8 +11,7 @@ import { OfertasService } from '../ofertas.service';
 })
 export class RestaurantesComponent implements OnInit {
 
-  public ofertas: Oferta[]
-  public dataTeste: any = new Date(2017, 10, 17)
+  public ofertas: Oferta[]  
 
   constructor(private ofertaService: OfertasService) { }
 
