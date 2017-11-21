f6db296b1f4653fc512ff08ba1e1ef9ead5a96df
Djavani <djavanirgomes@gmail.com>
2017-11-21 11:28:44 -0200

    Criando um serviço para ordens de compra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdemCompraService } from 'app/ordem-compra.service';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.css']
+  styleUrls: ['./ordem-compra.component.css'],
+  providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
 
@@ -27,9 +29,10 @@ export class OrdemCompraComponent implements OnInit {
   // controlar botao confirmar compra
   public formEstado: string = 'disabled';
 
-  constructor() { }
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
+    this.ordemCompraService.efetivarCompra()
   }
 
   public atualizaEndereco(endereco: string): void {
