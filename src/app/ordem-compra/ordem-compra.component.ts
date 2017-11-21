6666c09666175618ad01c857f535f56c3c1ff920
Djavani <djavanirgomes@gmail.com>
2017-11-21 17:36:00 -0200

    Feedback parte 2 - Aplicando a lógica

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -10,6 +10,8 @@ import { Pedido } from '../shared/pedido.model';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public idPedidoCompra: number
+
   // Pedido
   public pedido: Pedido = new Pedido('', '', '', '')
 
@@ -108,7 +110,10 @@ export class OrdemCompraComponent implements OnInit {
     this.pedido.complemento = this.complemento;
     this.pedido.formaPagamento = this.formaPagamento
 
-    this.ordemCompraService.efetivarCompra(this.pedido).subscribe()
+    this.ordemCompraService.efetivarCompra(this.pedido)
+      .subscribe(( idPedido: number) => {
+        this.idPedidoCompra = idPedido;
+    })
   }
 
 }
