25d3533c6c8f6c3c2e6d1d9cd863a0d525381775
Djavani Gomes <drgomes5@stefanini.com>
2017-12-05 11:09:06 -0200

    Carrinho de compras - Atribuindo CarrinhoService ao componente Oferta

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,36 +1,41 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router'
 import { OfertasService } from './../ofertas.service';
+import CarrinhoService from '../carrinho.service'
+
 import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
-  providers: [ OfertasService]
+  providers: [ OfertasService, CarrinhoService]
 })
 export class OfertaComponent implements OnInit {
-  
+
   public oferta: Oferta
 
   constructor(
-    private route: ActivatedRoute, 
-    private ofertasService: OfertasService
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService,
+    private carrinhoService: CarrinhoService
   ) { }
 
   ngOnInit() {
 
+    console.log('OrdemCompra - Array de itens do carrinho', this.carrinhoService.exibirItens())
+
     this.route.params.subscribe((parametros: Params) => {
-      
+
       this.ofertasService.getOfertaPorId(parametros.id)
       .then(( oferta: Oferta ) => {
         this.oferta = oferta
       })
-      
-    })        
-    
+
+    })
+
   }
-  
+
   ngOnDestroy(){
   }
 
